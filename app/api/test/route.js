e587b9dd5f760db374adc528019f88925d9d0379
Amelia <amelia.riding@sofology.co.uk>
2023-08-13 10:48:34 +0100

    Setup config

--- app/api/test/route.js
+++ app/api/test/route.js
@@ -1,5 +1,12 @@
 import { NextResponse } from "next/server";
 
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 export async function GET(request) {
   return NextResponse.json({ message: "Hello World" }, { status: 200 });
 }
